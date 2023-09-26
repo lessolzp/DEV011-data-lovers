03ea76a36de5d559372c1102cc67b5e20ab01900
Leslie <leslie@MacBook-Pro-de-Leslie.local>
2023-09-26 11:33:21 -0600

    hace los cambios necesarios en la función renderItems

--- src/view.js
+++ src/view.js
@@ -6,78 +6,112 @@ export const renderItems = (data) => {
   //Creación de elementos
   //Primero vamos a hacer la variable de referencia "padre"
   const container = document.getElementById("root");
+  //Empieza el bucle para generar los elementos de cada filme 
+  filteredData.forEach((element) => {
   // Crear un elemento <ul> con atributo itemscope y itemtype
   const ulElement = document.createElement("ul");
+  ulElement.setAttribute("class", "card");
   ulElement.setAttribute("itemscope", "");
   ulElement.setAttribute("itemtype", "http://schema.org/Movie");
   // Agregar el elemento <ul> al documento HTML con atributo itemprop
-  container.appendChild(ulElement);
-  //Empieza el bucle para generar los elementos de cada filme 
-  filteredData.forEach((element) => {
+  
     // Crear un elemento <li>
     const liElement = document.createElement("li");
+    liElement.setAttribute("class", "front");
+    //Crear el div para contenido
+    const divElement = document.createElement("div");
+    divElement.setAttribute("class","content");
     //Imagen
     const imgElement = document.createElement("img");
     imgElement.setAttribute("src", element.poster);
     imgElement.setAttribute("alt", " ");
-    // Crear un elemento <dl> con atributo itemscope y itemtype
-    const dlElement = document.createElement("dl");
-    dlElement.setAttribute("itemscope", "");
-    dlElement.setAttribute("itemtype", "http://schema.org/Movie");
     // Crear elementos <dl> y sus respectivos <dt> y <dd> con atributo itemscope y itemtype
+    //Crear otro elemento div para el título
+    const divTitle = document.createElement("div");
+    divTitle.setAttribute("class","title");
     //Titulo
+    const dlTitle = document.createElement("dl");
     const dtTitle = document.createElement("dt");
-    const ddTitle = document.createElement("dd");
-    ddTitle.setAttribute("itemprop", "title");
-    //Descripción
-    const dtDescription = document.createElement("dt");
-    const ddDescription = document.createElement("dd");
-    ddDescription.setAttribute("itemprop", "description");
+    dtTitle.setAttribute("itemprop", "title");
+    //crear el div para la info
+    const divInfo = document.createElement("div");
+    divInfo.setAttribute("class","info");
     //Director
+    const dlDirector = document.createElement("dl");
     const dtDirector = document.createElement("dt");
-    const ddDirector = document.createElement("dd");
-    ddDirector.setAttribute("itemprop", "director");
+    dtDirector.setAttribute("itemprop", "director");
     //Productor
+    const dlProducer = document.createElement("dl");
     const dtProducer = document.createElement("dt");
-    const ddProducer = document.createElement("dd");
-    ddProducer.setAttribute("itemprop", "producer");
+    dtProducer.setAttribute("itemprop", "producer");
+    //crear el div para la info en línea
+    const divLineInfo = document.createElement("div");
+    divLineInfo.setAttribute("class","inlineinfo");
     //Año lanzamiento
+    const dlrelDate = document.createElement("dl");
     const dtrelDate = document.createElement("dt");
-    const ddrelDate = document.createElement("dd");
-    ddrelDate.setAttribute("itemprop", "release_date");
+    dtrelDate.setAttribute("itemprop", "release_date");
     //Score
+    const dlrtScore = document.createElement("dl");
     const dtrtScore = document.createElement("dt");
-    const ddrtScore = document.createElement("dd");
-    ddrtScore.setAttribute("itemprop", "rt_score");
-    // Configurar los elementos <dt> y <dd> con los datos del objeto
-    dtTitle.innerHTML = "Película:";
-    ddTitle.innerHTML = element.title;
-    dtDescription.innerHTML = "Descripción:";
-    ddDescription.innerHTML = element.description;
-    dtDirector.innerHTML = "Director:";
-    ddDirector.innerHTML = element.director;
-    dtProducer.innerHTML = "Productor:";
-    ddProducer.innerHTML = element.producer;
-    dtrelDate.innerHTML = "Año de lanzamiento:";
-    ddrelDate.innerHTML = element.release_date;
-    dtrtScore.innerHTML = "Ranking:";
-    ddrtScore.innerHTML = element.rt_score;
+    dtrtScore.setAttribute("itemprop", "rt_score");
+    // Crear un elemento <li> para la parte de atras
+    const liBack = document.createElement("li");
+    liBack.setAttribute("class", "back");
+    //Crear el div para contenido
+    const divContentBack = document.createElement("div");
+    divContentBack.setAttribute("class","content");
+    //Crear el div para título trasero
+    const divTitleBack = document.createElement("div");
+    divTitleBack.setAttribute("class","title");
+    //Titulo
+    //const dtTitle = document.createElement("dt");
+    //dtTitle.setAttribute("itemprop", "title");
+    //Descripción
+    const dlDescription = document.createElement("dl");
+    const dtDescription = document.createElement("dt");
+    dtDescription.setAttribute("itemprop", "description");
+    // Configurar los elementos <dl> y <dt> con los datos del objeto
+    //dlTitle.innerHTML = "Película:";
+    dtTitle.innerHTML = element.title;
+    //dtDescription.innerHTML = "Descripción:";
+    dlDescription.innerHTML = element.description;
+    dlDirector.innerHTML = "Director:";
+    dtDirector.innerHTML = element.director;
+    dlProducer.innerHTML = "Producer:";
+    dtProducer.innerHTML = element.producer;
+    dlrelDate.innerHTML = "Release date:";
+    dtrelDate.innerHTML = element.release_date;
+    dlrtScore.innerHTML = "Ranking Position:";
+    dtrtScore.innerHTML = element.rt_score;
     //Agregar los elementos <li> y <dl> 
+    container.appendChild(ulElement);
     ulElement.appendChild(liElement);
-    liElement.appendChild(dlElement);
+    liElement.appendChild(divElement);
     // Agregar los elementos <dt> y <dd> al elemento <dl>
-    dlElement.appendChild(dtTitle);
-    dlElement.appendChild(ddTitle);
-    dlElement.appendChild(imgElement);
-    dlElement.appendChild(dtDescription);
-    dlElement.appendChild(ddDescription);
-    dlElement.appendChild(dtDirector);
-    dlElement.appendChild(ddDirector);
-    dlElement.appendChild(dtProducer);
-    dlElement.appendChild(ddProducer);
-    dlElement.appendChild(dtrelDate);
-    dlElement.appendChild(ddrelDate);
-    dlElement.appendChild(dtrtScore);
-    dlElement.appendChild(ddrtScore);
+    divElement.appendChild(imgElement);
+    divElement.appendChild(divTitle);
+    divTitle.appendChild(dlTitle);
+    divTitle.appendChild(dtTitle);
+    //Agregamos el div "info"
+    divElement.appendChild(divInfo);
+    divInfo.appendChild(dlDirector);
+    divInfo.appendChild(dtDirector);
+    divInfo.appendChild(dlProducer);
+    divInfo.appendChild(dtProducer);
+    //Agregamos el div "info in line"
+    divElement.appendChild(divLineInfo);
+    divLineInfo.appendChild(dlrelDate);
+    divLineInfo.appendChild(dtrelDate);
+    divLineInfo.appendChild(dlrtScore);
+    divLineInfo.appendChild(dtrtScore);
+    //Agregamos el li para la parte de atras
+    ulElement.appendChild(liBack);
+    liBack.appendChild(divContentBack);
+    divContentBack.appendChild(divTitleBack);
+    divTitleBack.appendChild(dlTitle);
+    divTitleBack.appendChild(dtTitle);
+    divContentBack.appendChild(dlDescription);
+    divContentBack.appendChild(dtDescription);
   });
 };
