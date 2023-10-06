8e4c0dc0cdc6978696ba0f47c5914ea8f20a2da6
Ohcatin <catalinajaviera.ordenes@gmail.com>
2023-10-06 10:13:36 -0300

    cambios pequeños de css, como cambio de tamaño texto, color, etc.

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,11 +1,28 @@
-// estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
+export const filterData = (data, filterBy, value) => {
+  const filteredData = data.films.filter((film)=> film[filterBy]=== value);
+  return filteredData;
 };
 
-export const anotherExample = () => {
-  return [];
+export const sortData = (dataItems, sortBy, sortOrder) => {
+  const copyData = [...dataItems];
+  const compare = (a, b) => {
+    const sortByA = parseFloat(a[sortBy]);
+    const sortByB = parseFloat(b[sortBy]);
+    if (sortOrder === 'asc') {
+      return sortByA > sortByB ? 1 : -1;
+    } else if (sortOrder === 'desc') {
+      return sortByB > sortByA ? 1 : -1;
+    }
+  };
+  const sortedData = copyData.sort(compare)
+  return sortedData;
+};
+
+export const peopleData =(film) =>{
+  const dataPeople = film.people.map((i)=>{
+    return i.name;
+  });
+  return dataPeople;
 };
 import { renderItems } from './view.js';
 //recibe los datos y los ordena según los parámetros especificados
